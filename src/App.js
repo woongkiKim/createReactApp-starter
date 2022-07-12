e16522738543f04a8ac2922daf3d6f2c16f5a2e4
woongkiKim <50348532+woongkiKim@users.noreply.github.com>
2022-07-12 09:25:16 +0900

    6.1 useEffect 한번만 실행하기 + useEffect(() => {}, []);

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 // import Button from "./Button";
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []);
+
   return (
     <div>
       <h1 className={styles.title}>{counter}</h1>
